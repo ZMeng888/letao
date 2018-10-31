e395e34dab7285945212bf6fe3ad86aff55a6191
zhangmengli <1139908067@qq.com>
2018-10-31 21:10:45 +0800

    首页树状图和饼图完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,5 @@
+
+
 // $(document).ajaxStart(function(){
 //   NProgress.start();
 // });
@@ -18,4 +20,32 @@ $(document).ajaxStop(function() {
   setTimeout(function() {
     NProgress.done();
   }, 500 );
+});
+
+$(function() {
+ $(".lt_aside .nav .category").click(function() {
+   $(this).next().stop().slideToggle();
+ });
+ $(".lt_topbar .icon_menu").click(function() {
+   $(".lt_aside").toggleClass("hidemenu");
+   $(".lt_main").toggleClass("hidemenu");
+   $(".lt_topbar").toggleClass("hidemenu");
+ });
+
+ $(".lt_topbar .icon_logout").click(function() {
+   $("#logoutModal").modal("show");
+ });
+ $("#logoutBtn").click(function() {
+   $.ajax({
+     type:"get",
+     url:"/employee/employeeLogout",
+     dataType:"json",
+     success:function(info){
+      console.log(info);
+      location.href = "login.html"
+      
+     }
+
+   })
+ })
 });
